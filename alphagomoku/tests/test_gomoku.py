af28b5a1893b392a866ec0de449686e21642fa93
fcuni <francesc.cunillera@gmail.com>
2024-07-11 19:57:27 +0100

    add tests for game

---
+++ alphagomoku/tests/test_gomoku.py
@@ -0,0 +1,48 @@
+from game.gomoku import GomokuGame
+from game.gomoku_utils import GridPosition, Move, PlayerEnum
+
+
+def get_test_game():
+    game = GomokuGame()
+    initial_move = Move(PlayerEnum.BLACK, GridPosition(0, 0))
+    game.reset(initial_move)
+    return game, initial_move
+
+
+def test_initialise_game():
+    game, init_move = get_test_game()
+    assert game.rule_stage == 0
+    assert game.game_data.winner is None
+    assert game.game_data.moves == [init_move]
+    assert game.board.size == (15, 15)
+
+
+def test_make_move_updates_game_data():
+    game, move = get_test_game()
+    assert game.game_data.moves == [move]
+    assert game.game_data.winner is None
+    assert game.current_player == PlayerEnum.WHITE
+
+
+def test_can_store_gamedata_and_clone_boards():
+    game, _ = get_test_game()
+    game.store_game_data("test.json")
+    game2 = GomokuGame.replay_game("test.json")
+    assert game2.game_data.moves == game.game_data.moves
+    assert game2.game_data.winner == game.game_data.winner
+    assert game2.current_player == game.current_player
+
+
+def check_winner():
+    game, _ = get_test_game()
+    game.make_move(Move(PlayerEnum.WHITE, GridPosition(0, 1)))
+    game.make_move(Move(PlayerEnum.BLACK, GridPosition(1, 0)))
+    game.make_move(Move(PlayerEnum.WHITE, GridPosition(0, 2)))
+    game.make_move(Move(PlayerEnum.BLACK, GridPosition(2, 0)))
+    game.make_move(Move(PlayerEnum.WHITE, GridPosition(0, 3)))
+    game.make_move(Move(PlayerEnum.BLACK, GridPosition(3, 0)))
+    game.make_move(Move(PlayerEnum.WHITE, GridPosition(0, 4)))
+    # The next move should win the game for black player with a vertical 5 in a row
+    has_winner = game.make_move(Move(PlayerEnum.BLACK, GridPosition(4, 0)))
+    assert (has_winner is True), "Game should have a winner for vertical win with black player"
+    assert game.game_data.winner == PlayerEnum.BLACK, "Winner should be black player"
--- alphagomoku/tests/test_gomoku_utils.py
+++ alphagomoku/tests/test_gomoku_utils.py
@@ -1,5 +1,24 @@
 import pytest
-from game.gomoku_utils import GomokuBoard, GridPosition, Move, PlayerEnum
+from game.gomoku_utils import GomokuBoard, GomokuCell, GridPosition, Move, PlayerEnum
+
+
+def test_grid_position():
+    position = GridPosition(5, 5)
+    assert position.x == 5
+    assert position.y == 5
+    assert position() == (5, 5)
+
+
+def test_initialise_cell():
+    cell = GomokuCell()
+    # Originally the cell should be empty (no player)
+    assert cell.get_player() is None, "Cell should be empty"
+    # Set the player to black and check if it is black
+    cell.set_player(PlayerEnum.BLACK)
+    assert cell.get_player() == PlayerEnum.BLACK, "Cell should be black"
+    # Try to set the player to white, which should raise an error
+    with pytest.raises(AssertionError):
+        cell.set_player(PlayerEnum.WHITE)
 
 
 def test_initialise_board_and_make_move():
@@ -13,10 +32,14 @@ def test_initialise_board_and_make_move():
     # Introduce a move and check cell
     move = Move(PlayerEnum.BLACK, GridPosition(1, 1))
     board.make_move(move)
-    assert board[(move.position)].get_player() == PlayerEnum.BLACK, "Cell should be black"
+    assert board[move.position].get_player() == PlayerEnum.BLACK, "Cell should be black"
     assert board[empty_position].get_player() is None, "Cell should be empty"
 
     # Make an invalid move
     with pytest.raises(AssertionError):
         impossible_position = GridPosition(20, 20)
         board.make_move(Move(PlayerEnum.WHITE, impossible_position))
+
+
+def test_():
+    pass
