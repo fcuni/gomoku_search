2aeb7dc800d8c6b55279a95499c25b21a3c08025
fcuni <francesc.cunillera@gmail.com>
2024-07-22 17:44:04 +0100

    move script folder

--- alphagomoku/match/scripts/run_random_match.py
+++ alphagomoku/match/scripts/run_random_match.py
@@ -1,9 +1,12 @@
 from evaluators.base_evaluator import DummyEvaluator
+from match.gomoku_match import GomokuMatch
 from players.random_player import RandomPlayer
 from rl_env.env import GomokuEnv, RenderMode
 
 if __name__ == '__main__':
     env = GomokuEnv(board_evaluator=DummyEvaluator(), render_mode=RenderMode.CMD)
-    player = RandomPlayer(env)
+    player_one = RandomPlayer
+    player_two = RandomPlayer
+    match = GomokuMatch(player_one, player_two, env)
 
-    player.play()
+    match.play()
